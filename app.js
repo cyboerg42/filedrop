57e985db034cce449d5c7f989bcb87b7a95a1adb
cyborg00222 <cyborg00222@users.noreply.github.com>
2018-09-21 11:28:25 +0200

    make http port configurable

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ var path = require('path');
 var download = require('./routes/download');
 var upload = require('./routes/upload');
 var index = require('./routes/index');
+var settings = require('../modules/settings.js');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/download', download);
 app.use('/upload', upload);
 
-var server = app.listen(8081, function() {
-    console.log('Server listening on port 8081');
+var server = app.listen(settings.get("http_port"), function() {
+    console.log('Server listening on port ' + settings.get("http_port"));
 });
